6f539d26a1672a46f276317207f017bab3fbd7e0
stephenlasky <stephenjlasky@gmail.com>
2017-09-24 15:01:57 -0400

    Initial commit.

---
+++ prob_pyramids/runThis.m
@@ -0,0 +1,17 @@
+function [G, L] = pyramidsGL(im, N)
+% [G, L] = pyramidsGL(im, N)
+% Creates Gaussian (G) and Laplacian (L) pyramids of level N from image im.
+% G and L are cell where G{i}, L{i} stores the i-th level of Gaussian and Laplacian pyramid, respectively. 
+
+end
+
+
+function displayPyramids(G, L)
+% Displays intensity and fft images of pyramids
+
+end
+
+function displayFFT(im, minv, maxv)
+% Displays FFT images
+
+end
