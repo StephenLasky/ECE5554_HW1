7d7248be350ac23defc2ead4a77b63362ef3364a
stephenlasky <stephenjlasky@gmail.com>
2017-09-30 17:05:47 -0400

    Preparing to add the rotational ability to the gaussian derivative.

--- prob_edge/edgeGradient.m
+++ prob_edge/edgeGradient.m
@@ -6,47 +6,108 @@ sigma = 1;
 [mag, thetas] = gradientMagnitude(im,sigma);
 [rows, cols] = size(mag);
 
-bmap = zeros(rows,cols);
+figure(); imshow(mag);
 
+bmap = nonmax(mag,thetas);
 
-for x = 1:rows
-    for y = 1:cols
-        theta = thetas(x,y);
-        
-        pp = 0.0;   % pixel in positive direction
-        np = 0.0;   % pixel in negative direction
-        
-        theta = theta * 2;
-        if theta >= 0.0 && theta < pi/4
-            pp = cos(theta)*mag(y,x+1) + sin(theta)*mag(y+1,x+1);
-            theta = theta - 2;
-            np = cos(theta)*mag(y,x-1) + sin(theta)*mag(y-1,x-1);
-        elseif theta >= pi/4 && theta < pi/2
-            pp = sin(theta)*mag(y+1,x+1) -cos(theta)*mag(y+1,x);
-            theta = theta - 2;
-            np = sin(theta)*mag(y-1,x-1) -cos(theta)*mag(y-1,x);
-        elseif theta >= pi/pi && theta < 0.75*pi
-            pp = -cos(theta)*mag(y+1,x) - sin(theta)*mag(y+1,x-1);
-            theta = theta - 2;
-            np = -cos(theta)*mag(y-1,x) - sin(theta)*mag(y-1,x+1);
-        elseif theta >= 0.75*pi && theta <= pi
-            pp = -sin(theta)*mag(y+1,x-1) + cos(theta)*mag(y,x-1);
-            theta = theta - 2;
-            np = -sin(theta)*mag(y-1,x+1) + cos(theta)*mag(y,x+1);
-        end
-        
-%         elseif theta >= -pi && theta < -0.75*pi
-%         elseif theta >= -0.75*pi && theta < -pi/2
-%         elseif theta >= -pi/2 && theta < -pi/4
-%         elseif theta >= -pi/4 && theta < 0.0
-                    
-        
-        
-        
-        
-        
-    end
-end
+% NOTE: must deal with edge cases later
+% for x = 2:rows-1
+%     for y = 2:cols-1
+%         theta = thetas(x,y);
+%         
+% %         cp = mag(x,y);  % current pixel
+% %         pp = 0.0;       % pixel in positive direction
+% %         np = 0.0;       % pixel in negative direction
+%         
+% %         if theta < 0
+% %             theta = -1 * theta;
+% %         end
+% %         
+% %         if theta >= 0.0 && theta < pi/4
+% %             theta = theta * 2;
+% %             pp = cos(theta)*mag(y,x+1) + sin(theta)*mag(y+1,x+1);
+% %             theta = theta - 2*pi;
+% %             np = cos(theta)*mag(y,x-1) + sin(theta)*mag(y-1,x-1);
+% %         elseif theta >= pi/4 && theta < pi/2
+% %             theta = theta * 2;
+% %             pp = sin(theta)*mag(y+1,x+1) -cos(theta)*mag(y+1,x);
+% %             theta = theta - 2*pi;
+% %             np = sin(theta)*mag(y-1,x-1) -cos(theta)*mag(y-1,x);
+% %         elseif theta >= pi/pi && theta < 0.75*pi
+% %             theta = theta * 2;
+% %             pp = -cos(theta)*mag(y+1,x) - sin(theta)*mag(y+1,x-1);
+% %             theta = theta - 2*pi;
+% %             np = -cos(theta)*mag(y-1,x) - sin(theta)*mag(y-1,x+1);
+% %         elseif theta >= 0.75*pi && theta <= pi
+% %             theta = theta * 2;
+% %             pp = -sin(theta)*mag(y+1,x-1) + cos(theta)*mag(y,x-1);
+% %             theta = theta - 2*pi;
+% %             np = -sin(theta)*mag(y-1,x+1) + cos(theta)*mag(y,x+1);
+% %         else
+% %             disp('edgeGradient: theta not accepted.');
+% %         end
+%         
+% %         theta = round(theta / (pi/4)) * (pi/4);
+% %         theta = int32(round(theta,2)*10);
+% %         zero = 0;
+% %         pi_4 = int32(round(pi/4,2)*10);
+% %         pi_2 = int32(round(pi/2,2)*10);
+% %         pi_34 = int32(round(3*pi/4,2)*10);
+% %         pii = int32(round(pi)*10);
+% %         if theta == zero
+% %             pp = mag(y,x+1);
+% %             np = mag(y,x-1);
+% %         elseif theta == pi_4
+% %             pp = mag(y+1,x+1);
+% %             np = mag(y-1,x-1);
+% %         elseif theta == pi_2
+% %             pp = mag(y+1,x);
+% %             np = mag(y-1,x);
+% %         elseif theta == 3pi_4
+% %             pp = mag(y+1,x-1);
+% %             np = mag(y,x-1);
+% %         elseif theta == pii
+% %             pp = mag(y,x-1);
+% %             np = mag(y,x+1);
+% %         elseif theta == -pi_4
+% %             pp = mag(y-1,x+1);
+% %             np = mag(y+1,x-1);
+% %         elseif theta == -pi_2
+% %             pp = mag(y-1,x);
+% %             np = mag(y+1,x);
+% %         elseif theta == -pi_34
+% %             pp = mag(y-1,x-1);
+% %             np = mag(y+1,x+1);
+% %         elseif theta == -pii
+% %             pp = mag(y,x-1);
+% %             np = mag(y,x+1);
+% %         else
+% %             disp('edgeGradient: theta not accepted.');
+% %             theta
+% %         end
+% %             
+% %         
+% %         
+% %         
+% %         if cp > pp && cp > np
+% %             bmap(x,y) = 0;
+% %         end
+%         
+%         
+%         
+%         
+% %         elseif theta >= -pi && theta < -0.75*pi
+% %         elseif theta >= -0.75*pi && theta < -pi/2
+% %         elseif theta >= -pi/2 && theta < -pi/4
+% %         elseif theta >= -pi/4 && theta < 0.0
+%                     
+%         
+%         
+%         
+%         
+%         
+%     end
+% end
 
 
 
--- prob_edge/test.m
+++ prob_edge/test.m
@@ -1,26 +1,19 @@
 % im = im2single(imread('data/images/101085.jpg'));
 im = im2single(imread('swedishflag.jpg'));
 % im = rgb2gray(im);
-
-% figure(); imshow(im);
-
 [mag, theta] = gradientMagnitude(im,1);
 
-highest = -100;
-for x = 1:374
-    for y = 1:374
-        if theta(x,y) > highest
-            highest = theta(x,y);
-        end
-    end
-end
-disp(highest);
-% figure(); imshow(mag+0.5);
+im = nonmax(mag,theta);
 
-% x = edge(im,'canny');
-% 
-% figure(); imshow(x);
+figure(); imshow(im.^0.5);
 
+
+
+% bmap = edgeGradient(im);
+
+
+
+% figure(); imshow(bmap);
 
 
 
