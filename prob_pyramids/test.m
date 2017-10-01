86cec4a40d97849d18bbb20c1c481106a9c5d16c
stephenlasky <stephenjlasky@gmail.com>
2017-10-01 00:56:46 -0400

    Second 'final' commit. Answered an additional grad problem.

--- prob_pyramids/test.m
+++ prob_pyramids/test.m
@@ -1,11 +1,25 @@
 % Primary Test Code %
 im = rgb2gray(im2single(imread('tesla.jpg')));
-im = gaussian_filter(im,1);
-im = downsample(im);
+% im = gaussian_filter(im,1);
+% im = downsample(im);
 im = gaussian_filter(im,1);
 im = downsample(im);
 [G,L] = pyramidsGL(im,5);
-displayPyramids(G,L);
+% displayPyramids(G,L);
+
+figure(); imshow(im);
+
+% % im = L{1,1};
+% high_freq = 2*L{1,1};
+% % low_freq = G{1,1};
+% low_freq = gaussian_filter(upsample(im),1);
+% 
+% im = im + high_freq;
+% 
+% 
+% figure(); imshow(high_freq);
+% figure(); imshow(low_freq);
+% figure(); imshow(im);
 
 % Other test Code %
 % im = rgb2gray(im2single(imread('tesla.jpg'))); % get image
@@ -13,6 +27,8 @@ displayPyramids(G,L);
 % im = downsample(im);
 % [G,L] = pyramidsGL(im,3);
 % displayPyramids(G,L);
+
+
 
 
 
